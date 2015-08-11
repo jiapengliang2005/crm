d8b96f5108ea621e4365e292fb171a98cdbe058b
jiapengliang2005 <jiapengliang2005@163.com>
2015-08-11 10:25:27 +0800

    Update Project.java

--- src/crmgg/Project.java
+++ src/crmgg/Project.java
@@ -4,14 +4,14 @@ public class Project {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println("master");
 		System.out.println("fdfdfdfd");
 		
 		System.out.println("fdfdfdfd");
 		
 		System.out.println("fdfdfdfd");
 
-System.out.println("55555");
+System.out.println("1111111111111111111111111111111");
 
 	}
 
